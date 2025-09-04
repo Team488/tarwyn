c963f02a8306728333a64c813a179445d157fe34
Jun Jie Huang <1juhuang2@gmail.com>
2025-09-03 19:49:08 -0700

    ci(fix): update release workflow to use new versioning and archiving scripts

---
+++ scripts/get-version.sh
@@ -0,0 +1,9 @@
+# MIT License https://github.com/rojo-rbx/rokit/blob/main/LICENSE.txt
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CLI_MANIFEST=$(cargo read-manifest --manifest-path tarwyn/Cargo.toml)
+CLI_VERSION=$(echo $CLI_MANIFEST | jq -r .version)
+
+echo $CLI_VERSION
