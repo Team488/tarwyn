c159abc36b5deb41b1c66721936a0245d4cb086b
Lawrence Wu <yiwu0819@gmail.com>
2026-09-04 23:37:17 -0700

    chore: drop the stale guideline marker from the msgpack module

--- core/src/websocket/msgpack.rs
+++ core/src/websocket/msgpack.rs
@@ -4,8 +4,6 @@
 //! (<https://github.com/msgpack/msgpack/blob/master/spec.md>). Only the subset
 //! NT4 needs is implemented: ints, floats, str, bin, bool, nil, and arrays.
 
-// Rust guideline compliant 2026-02-21
-
 use std::fmt;
 
 use serde_json::{Map, Value};
